a4055f690c3891c25e463951aaec9965804d967e
Luger24 <luigipena23@gmail.com>
2022-08-16 04:01:22 -0600

    Agregar archivos de proyecto.

---
+++ ejercicio3_1/App.xaml.cs
@@ -0,0 +1,48 @@
+﻿using ejercicio3_1.ControllerSQLite;
+using System;
+using System.IO;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ejercicio3_1
+{
+    public partial class App : Application
+    {
+
+        static DataBase db;
+
+        public static DataBase DBase
+        {
+            get
+            {
+                if (db == null)
+                {
+                    String FolderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Empleado.db3");
+                    db = new DataBase(FolderPath);
+                }
+
+                return db;
+            }
+        }
+
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new MainPage();
+            MainPage = new NavigationPage(new Views.ListEmpleadoView());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ ejercicio3_1/ViewModels/ListEmpleadoViewModel.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Windows.Input;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+using ejercicio3_1.Models;
+
+using Xamarin.Forms;
+using System.Collections.Generic;
+
+namespace ejercicio3_1.ViewModels
+{
+    public class ListEmpleadoViewModel : BaseViewModel
+    {
+
+        private ObservableCollection<Empleado> _empleados;
+
+        public ObservableCollection<Empleado> Empleados
+        {
+            get { return _empleados; }
+            set { _empleados = value; OnPropertyChanged(); }
+        }
+
+        private Empleado _selectedEmpleado;
+
+        public Empleado SelectedEmpleado
+        {
+            get { return _selectedEmpleado; }
+            set { _selectedEmpleado = value; OnPropertyChanged(); }
+        }
+
+        public ICommand GoToDetailsCommand { private set; get; }
+        public ICommand CrearEmpleadoCommand { private set; get; }
+
+        public INavigation Navigation { get; set; }
+
+        public ListEmpleadoViewModel(INavigation navigation)
+        {
+            Navigation = navigation;
+            GoToDetailsCommand = new Command<Type>(async (pageType) => await GoToDetails(pageType));
+            CrearEmpleadoCommand = new Command<Type>(async (pageType) => await CrearEmpleado(pageType));
+
+
+
+            /*Empleados.Add(new Empleado() { Nombre = "Empleado 1", Apellidos = "Apellidos", Edad = 30, Direccion = "Dir. Emple 1", Puesto = "Ingeniero", Foto = null });
+            Empleados.Add(new Empleado() { Nombre = "Empleado 2", Apellidos = "Apellidos", Edad = 30, Direccion = "Dir. Emple 2", Puesto = "Ingeniero", Foto = null });
+            Empleados.Add(new Empleado() { Nombre = "Empleado 3", Apellidos = "Apellidos", Edad = 30, Direccion = "Dir. Emple 3", Puesto = "Ingeniero", Foto = null });*/
+
+
+        }
+
+        public async void setearEmpleadosSQLite()
+        {
+            Empleados = new ObservableCollection<Empleado>();
+
+            List<Empleado> ListEmpleados = new List<Empleado>();
+            ListEmpleados = await App.DBase.obtenerListaEmpleado();
+
+            if(ListEmpleados != null)
+            {
+                ListEmpleados.Reverse();
+
+                for (int i = 0; i < ListEmpleados.Count; i++)
+                {
+                    Empleados.Add(ListEmpleados[i]);
+                }
+            }
+        }
+
+        async Task GoToDetails(Type pageType)
+        {
+            if (SelectedEmpleado != null)
+            {
+                var page = (Page)Activator.CreateInstance(pageType);
+
+                page.BindingContext = new EmpleadoViewModel()
+                {
+                    Id = SelectedEmpleado.Id,
+                    Nombre = SelectedEmpleado.Nombre,
+                    Apellidos = SelectedEmpleado.Apellidos,
+                    Edad = SelectedEmpleado.Edad,
+                    Direccion = SelectedEmpleado.Direccion,
+                    Puesto = SelectedEmpleado.Puesto,
+                    Foto = SelectedEmpleado.Foto,
+
+                    btncrearIsVisible = false,
+                    btnactualizarIsVisible = true,
+                    btneliminarIsVisible = true,
+
+                    Navigation = Navigation
+                };
+
+                await Navigation.PushAsync(page);
+            }
+        }
+
+        async Task CrearEmpleado(Type pageType)
+        {
+            var page = (Page)Activator.CreateInstance(pageType);
+
+            page.BindingContext = new EmpleadoViewModel()
+            {
+                btncrearIsVisible = true,
+                btnactualizarIsVisible = false,
+                btneliminarIsVisible = false,
+
+                Navigation = Navigation
+            };
+
+            await Navigation.PushAsync(page);
+        }
+
+    }
+}
